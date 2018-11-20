a97851c4728aceea3d1a4fcd896e6ef8b2540c82
ashok.kumar@bdb.ai <manashee@gmail.com>
2018-11-20 14:43:11 +0530

    added Rand module

---
+++ src/Rand.elm
@@ -0,0 +1,3 @@
+module Rand exposing (..)
+
+rand  = 1
--- src/main.elm
+++ src/main.elm
@@ -1,4 +1,6 @@
 module Spell exposing (..)
 import Html exposing (text)
+import Rand exposing (rand)
 
-main = text "hello"
+main = text ("hello" ++ String.fromInt rand)
+
