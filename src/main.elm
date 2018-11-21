5dd75a39699ce08671f7fadb5112d43fdd1717e3
ashok.kumar@bdb.ai <manashee@gmail.com>
2018-11-21 18:07:06 +0530

    loaded novel from server and split into words

--- src/main.elm
+++ src/main.elm
@@ -3,14 +3,15 @@ import Browser
 import Rand exposing (randNum)
 import Random
 import Html.Events exposing (onClick)
+import Http 
 
-type Msg = RightAnswer | NextWord Int | NextPuzzle
+type Msg = RightAnswer | NextWord Int | NextPuzzle | DataReceived (Result Http.Error String)
 type alias Model = { randomNumber : Int , message : String , words : List String }
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( Model 1 "" []
-  , Cmd.none
+  ( Model 1 "" ["ashok","kumar"]
+  , Http.send DataReceived (Http.getString url)
   )
 
 url = "http://localhost:8001/words.txt"
@@ -21,12 +22,17 @@ update msg model =
                 NextPuzzle -> (model, Random.generate NextWord (randNum 100) )
                 NextWord randomIndex -> ({ randomNumber = randomIndex ,  message = "Choosing Puzzle", words = model.words } , Cmd.none)
                 RightAnswer -> ({ randomNumber = 0, message = "You win", words = model.words } , Cmd.none)
+                DataReceived (Ok sentences) -> let words = String.split " " sentences in ({model| words = words}, Cmd.none)
+                DataReceived (Err _) ->  (model , Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
 
 view : Model -> Html Msg
-view model = div[][ h1[] [ text (model.message ++ " " ++ String.fromInt model.randomNumber) ] , button [ onClick NextPuzzle ] [text  "Next Puzzle"] ] 
+view model = div[][ h1[] [ text (model.message ++ " " ++ String.fromInt model.randomNumber) ] , button [ onClick NextPuzzle ] [text  "Next Puzzle"] , ul [] (List.map viewWord model.words)  ] 
+
+viewWord : String -> Html Msg
+viewWord word = li [] [text word]
 
 main = Browser.element { init = init , view = view, update = update , subscriptions = subscriptions }
