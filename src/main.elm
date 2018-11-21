ffe870e0e2dea967662723c82073887040d35ddd
ashok.kumar@bdb.ai <manashee@gmail.com>
2018-11-21 06:44:36 +0530

    reorganized update and added words

--- src/main.elm
+++ src/main.elm
@@ -5,20 +5,22 @@ import Random
 import Html.Events exposing (onClick)
 
 type Msg = RightAnswer | NextWord Int | NextPuzzle
-type alias Model = { randomNumber : Int , message : String }
+type alias Model = { randomNumber : Int , message : String , words : List String }
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( Model 1 ""
+  ( Model 1 "" []
   , Cmd.none
   )
+
+url = "http://localhost:8001/words.txt"
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
         case msg of
                 NextPuzzle -> (model, Random.generate NextWord (randNum 100) )
-                NextWord randomIndex -> (Model randomIndex "Choosing puzzle ... " , Cmd.none) 
-                RightAnswer -> (Model 0 "You win", Cmd.none)
+                NextWord randomIndex -> ({ randomNumber = randomIndex ,  message = "Choosing Puzzle", words = model.words } , Cmd.none)
+                RightAnswer -> ({ randomNumber = 0, message = "You win", words = model.words } , Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
