088bd680eb3cb7584a0f3c75f388f88b30b0a0e4
ashok.kumar@bdb.ai <manashee@gmail.com>
2018-11-22 07:06:59 +0530

    simplified update

--- src/main.elm
+++ src/main.elm
@@ -1,10 +1,12 @@
 import Html exposing (..)
-import Browser
+import Browser 
+import Browser.Events exposing (onKeyUp) 
 import Rand exposing (randNum)
 import Random
 import Html.Events exposing (onClick)
 import Http 
 import Array
+
 type Msg = RightAnswer | NextWord Int | NextPuzzle | DataReceived (Result Http.Error String)
 type alias Model = { randomNumber : Int , message : String , words : List String }
 
@@ -20,8 +22,8 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
         case msg of
                 NextPuzzle -> (model, Random.generate NextWord (randNum 100) )
-                NextWord randomIndex -> ({ randomNumber = randomIndex ,  message = "Choosing Puzzle", words = model.words } , Cmd.none)
-                RightAnswer -> ({ randomNumber = 0, message = "You win", words = model.words } , Cmd.none)
+                NextWord randomIndex -> ({ model | randomNumber = randomIndex } , Cmd.none)
+                RightAnswer -> ({ model | message = "You win"} , Cmd.none)
                 DataReceived (Ok sentences) -> let words = String.split " " sentences in ({model| words = words}, Cmd.none)
                 DataReceived (Err _) ->  (model , Cmd.none)
 
@@ -31,7 +33,6 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model = div[][ h1[] (viewWord model.randomNumber model) , button [ onClick NextPuzzle ] [text  "Next Puzzle"] ] 
-
 viewWord idx model = [text (getWord idx model.words)]
 getWord idx words = Array.get idx (Array.fromList words) |> Maybe.withDefault "black"
 
